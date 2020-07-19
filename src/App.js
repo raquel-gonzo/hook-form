b3555be891fe0062c808cbeb1704e9f3cde3a2db
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-07-19 14:11:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Form from '../src/components/Form';
 import './App.css';
+import Results from '../src/components/Results';
 
 function App() {
+const [state, setState] = useState({    // sets the state higher in scope to 
+  firstName:"",
+  lastName:"",
+  email: "",
+  password: "",
+  confirmPassword: "",
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form inputs={state} setInputs={setState}/>
+      <Results data={state} />
     </div>
   );
 }
